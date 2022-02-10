edcf7a35c268835ad0f1090e719b60c5a179184f
Amanda971 <mmefabriano@gmail.com>
2022-02-10 08:25:49 +0100

    c

---
+++ script.js
@@ -0,0 +1,8 @@
+
+var nom= prompt ("Hey what's ur name ?!");
+if (nom!= null) {
+alert("hey! " + nom +  " nice too meet u ")
+
+document.getElementById("Coucou").innerHTML =
+  "Super! "  + nom +  " sois la/le bienvenue ." ;
+}
